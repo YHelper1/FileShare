78a6849c929e1fa05a65e2e6f05db7a00b1fca00
Nikita <nik.kiselev.2010@inbox.ru>
2024-11-08 10:59:22 +0400

    1

--- client.py
+++ client.py
@@ -1,13 +1,23 @@
-import socket, start_client, PyQt6.QtWidgets, sys, PyQt6.QtGui
-import pathlib, connect_
-from threading import Thread
+import socket, start_client, PyQt6.QtWidgets, sys, PyQt6.QtGui, progress_client, PyQt6.QtCore
+import pathlib, os
+import threading
+import search_ui
+import PyQt6, time
+from PyQt6.QtCore import Qt
+
+
 def read_props():
-    f = open("properties.txt", "r")
-    mas = []
-    for i in f.readlines():
-        mas.append(i[i.find(" ") + 1:].rstrip('\n'))
-    f.close()
-    return mas
+    try:
+        f = open("properties.txt", "r")
+        mas = []
+        for i in f.readlines():
+            mas.append(i[i.find(" ") + 1:].rstrip('\n'))
+        f.close()
+        return mas
+    except FileNotFoundError:
+        f = open("properties.txt", "w")
+        f.write("ip: localhost\nport: 80")
+        return ["localhost", 80]
 
 
 class settings_window(PyQt6.QtWidgets.QMainWindow):
@@ -39,7 +49,6 @@ class settings_window(PyQt6.QtWidgets.QMainWindow):
         self.confirm.setText("Готово")
         self.confirm.clicked.connect(self.conf)
 
-
     def conf(self):
 
         if self.port.text() and self.ip.text():
@@ -66,35 +75,132 @@ class start_window(PyQt6.QtWidgets.QMainWindow, start_client.Ui_MainWindow):
         self.settings.resize(40, 40)
         self.settings.move(420, 0)
         self.settings.clicked.connect(self.open_settings)
-        self.pushButton.clicked.connect(self.load)
+        self.pushButton.clicked.connect(self.upload)
+        self.pushButton_2.clicked.connect(self.get)
 
     def open_settings(self):
         self.wind = settings_window()
         self.wind.show()
 
-    def load(self):
-        self.loadwind = load_file()
-        self.loadwind.show()
+    def upload(self):
+        self.uploadwind = load_file()
+        self.uploadwind.show()
 
-class load_file(PyQt6.QtWidgets.QMainWindow):
+    def get(self):
+        self.getfilewind = get_file()
+        self.getfilewind.show()
+
+
+
+class load_file(PyQt6.QtWidgets.QMainWindow, progress_client.Ui_MainWindow):
     def __init__(self):
         super().__init__()
+        super().setupUi(self)
+
         self.setWindowTitle("Загрузка файла")
-        self.setFixedSize(300, 200)
-        self.progress_bar = PyQt6.QtWidgets.QProgressBar(self)
-        self.progress_bar.move(0, 50)
-        #self.progress_bar.setRange(0, 100)
-        Thread(self.send_f()).run()
+        self.setFixedSize(self.size())
+        self.Done_button.hide()
+        self.progressBar.setRange(0, 100)
+        self.progressBar.setValue(0)
+        self.Done_button.clicked.connect(self.close)
+        self.pass_lbl = PyQt6.QtWidgets.QLabel(self)
+        self.pass_lbl.move(120, 55)
+        self.pass_lbl.hide()
+        threading.Thread(self.send_f()).run()
+
     def send_f(self):
-        file = PyQt6.QtWidgets.QFileDialog.getOpenFileName(self)[0]
-        input_file = open(file, "rb")
-        data = input_file.read()
-        print(len(data))
-        input_file.close()
+        SEPARATOR = "<"
+        BUFFER_SIZE = 4096
+        self.file = PyQt6.QtWidgets.QFileDialog.getOpenFileName(self)[0]
+        self.show()
+        PyQt6.QtWidgets.QApplication.processEvents()
+        filesize = os.path.getsize(self.file)
         ip = read_props()[0]
         port = read_props()[1]
-        connection = connect_.connect_(ip, int(port))
-        connection.sendfile(data)
+        step = 100 / (filesize / BUFFER_SIZE)
+        now = 0
+        connection = socket.socket()
+        connection.connect((ip, int(port)))
+        info = str(self.file) + SEPARATOR + str(filesize)
+        PyQt6.QtWidgets.QApplication.processEvents()
+        if len(str.encode(info)) < 4096:
+            info += (" " * (4096 - len(str.encode(info))))
+            connection.sendall(str.encode(info))
+        else:
+            connection.send(f"{self.file}{SEPARATOR}{filesize}".encode())
+        with open(self.file, "rb") as f:
+            while True:
+                PyQt6.QtWidgets.QApplication.processEvents()
+                bytes_read = f.read(BUFFER_SIZE)
+                if not bytes_read:
+                    break
+                connection.sendall(bytes_read)
+                now += step
+                if now > 1:
+                    self.progressBar.setValue(self.progressBar.value() + int(now))
+                    now -= int(now)
+
+        passcode = connection.recv(8).decode()
+        self.Status_lbl.setText("Файл загружен")
+        self.pass_lbl.setFixedSize(200, 30)
+        self.pass_lbl.setTextInteractionFlags(Qt.TextInteractionFlag.TextSelectableByMouse)
+        self.pass_lbl.setText(f"Код файла: {passcode}")
+        self.pass_lbl.show()
+        self.Done_button.show()
+        connection.close()
+
+class get_file(PyQt6.QtWidgets.QMainWindow,search_ui.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        super().setupUi(self)
+        self.download_btn.hide()
+        self.search_btn.clicked.connect(self.search)
+        self.download_btn.setIcon(PyQt6.QtGui.QIcon("download.png"))
+        self.download_btn.clicked.connect(self.download)
+
+    def search(self):
+        SEPARATOR = "<"
+        BUFFER_SIZE = 4096
+        ip = read_props()[0]
+        port = read_props()[1]
+        self.connection = socket.socket()
+        self.connection.connect((ip, int(port)))
+        info = "%SEARCH%<" + self.search_line.text().replace(" ", "")
+        if len(str.encode(info)) < 4096:
+            info += (" " * (4096 - len(str.encode(info))))
+            self.connection.sendall(str.encode(info))
+        else:
+            self.connection.sendall(str.encode(info))
+        self.filename = self.connection.recv(4096).decode()
+        self.file_name.setText(self.filename)
+        self.download_btn.show()
+
+    def download(self):
+        HOME_PATH = os.getenv("HOME")
+        dir_path = PyQt6.QtWidgets.QFileDialog.getExistingDirectory(
+            parent=self,
+            caption="Select directory",
+            directory=HOME_PATH,
+            options=PyQt6.QtWidgets.QFileDialog.Option.DontUseNativeDialog,
+        )
+        print(f"{dir_path}/{self.filename}")
+        info = "%download%<" + self.filename
+        if len(str.encode(info)) < 4096:
+            info += (" " * (4096 - len(str.encode(info))))
+            self.connection.sendall(str.encode(info))
+        else:
+            self.connection.send(info.encode())
+        BUFFER_SIZE = 4096
+        elem1 = os.path.basename(f"{dir_path}/{self.filename}")
+        with open(elem1, "wb") as f:
+            while True:
+
+                bytes_read = self.connection.recv(BUFFER_SIZE)
+                if len(bytes_read) < BUFFER_SIZE:
+                    break
+                f.write(bytes_read)
+                print(1)
+
 
 def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
--- server.py
+++ server.py
@@ -1,34 +1,79 @@
-import socket
+import socket, os
 from threading import Thread
+import sqlite3
+import uuid
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # bind the socket to a public host, and a well-known port
 serversocket.bind(('localhost', 80))
 # become a server socket
 serversocket.listen(5)
 
+con = sqlite3.connect("files.sqlite")
+cur = con.cursor()
 
-def recvall(sock):
-    print('connected')
-    bs = 4096
-    data = bytearray()
+def recvall(sock, address):
+    BUFFER_SIZE = 4096
+    SEPARATOR = "<"
+    received = client_socket.recv(BUFFER_SIZE).decode()
+    elem1, elem2 = received.split(SEPARATOR)
+    if elem1 == "%SEARCH%":
+        send_info(elem2.replace(" ", ""), sock)
+    elif elem1 == "%download%":
+        send_f(client_socket, elem2)
+    else:
+        work_dir = os.getcwd()
+        if not os.path.exists(f"{work_dir}/{address[0]}"):
+            os.makedirs(f"{work_dir}/{address[0]}")
+
+        elem1 = os.path.basename(f"{work_dir}/{address[0]}/{elem1}")
+        with open(elem1, "wb") as f:
+            while True:
+                bytes_read = client_socket.recv(BUFFER_SIZE)
+                if len(bytes_read) < BUFFER_SIZE:
+                    break
+                f.write(bytes_read)
+        return_passcode(sock, address[0], elem1)
+    sock.close()
+
+
+def return_passcode(sock: socket.socket, folder, name):
     while True:
-        part = sock.recv(bs)
-        data.extend(part)
-        if len(part) < bs:
+        uid = str(uuid.uuid4())[:8]
+        print(uid)
+        result = cur.execute(f"select path from path_n_uid where uid = '{uid}'").fetchone()
+        if not result:
+            cur.execute(f"INSERT INTO path_n_uid (uid, path) VALUES ('{uid}', '{folder + '/' + name}')")
+            con.commit()
             break
-    print(len(data))
-    return data
+    sock.sendall(uid.encode())
 
-def write_file(sock):
-    data = recvall(sock)
-    out = open('test_recieved/output.zip', "wb")
-    out.write(data)
-    out.close()
+
+def send_info(passcode, sock: socket.socket):
+    result = cur.execute(f"select path from path_n_uid where uid = '{passcode}'").fetchone()
+    print(result)
+    if not result:
+        sock.sendall(str.encode((" " * 4096)))
+    else:
+        file = str(result[0])
+        filename = file[file.rfind("/") + 1:]
+        sock.sendall(str.encode(filename) + str.encode((" " * (4096 - len(str.encode(filename))))))
+
+def send_f(connection: socket.socket, file):
+    BUFFER_SIZE = 4096
+    with open(file, "rb") as f:
+        print(file)
+        while True:
+            bytes_read = f.read(BUFFER_SIZE)
+            if not bytes_read:
+                break
+            connection.sendall(bytes_read)
+            print(2)
+
 while True:
     # accept connections from outside
-    (clientsocket, address) = serversocket.accept()
+    (client_socket, address) = serversocket.accept()
     if address:
-        Thread(write_file(clientsocket)).run()
+        Thread(recvall(client_socket, address)).run()
 
     # now do something with the clientsocket
 
